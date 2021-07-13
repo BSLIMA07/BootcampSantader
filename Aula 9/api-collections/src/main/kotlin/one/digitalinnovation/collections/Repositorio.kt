9109a70828c2aa4dde73c83308b34137ce775f02
BSLIMA07 <bslima07@gmail.com>
2021-07-13 17:07:31 -0300

    created mutableMapOf2 test and Repositorio class

--- Aula 9/api-collections/src/main/kotlin/one/digitalinnovation/collections/Repositorio.kt
+++ Aula 9/api-collections/src/main/kotlin/one/digitalinnovation/collections/Repositorio.kt
@@ -6,7 +6,9 @@ class Repositorio<T> {
     fun create(id: String, value: T){
         map[id] = value
     }
+    fun remove(id: String) = map.remove(id)
 
     fun findByid(id: String) = map[id]
 
+    fun findAll() = map.values
 }

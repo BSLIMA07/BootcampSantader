0b35d9a5c8c597ffed7651b27965df4ff745cc30
BSLIMA07 <bslima07@gmail.com>
2021-07-13 16:51:18 -0300

    created mutableMapOf test and Repositorio class

---
+++ Aula 9/api-collections/src/main/kotlin/one/digitalinnovation/collections/Repositorio.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.collections
+
+class Repositorio<T> {
+    private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T){
+        map[id] = value
+    }
+
+    fun findByid(id: String) = map[id]
+
+}
